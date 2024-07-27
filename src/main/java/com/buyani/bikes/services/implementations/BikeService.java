93a62f08e15ca919f23d65d1884519aa0bccd338
buyanimhlongo@gmail.com <buyani.mhlongo@palota.co.za>
2024-07-27 17:31:13 +0200

    get bike by id

--- src/main/java/com/buyani/bikes/services/implementations/BikeService.java
+++ src/main/java/com/buyani/bikes/services/implementations/BikeService.java
@@ -4,11 +4,10 @@ import com.buyani.bikes.models.dao.Bike;
 import com.buyani.bikes.models.dto.BikeDTO;
 import com.buyani.bikes.repositories.IBikeRepository;
 import com.buyani.bikes.services.interfaces.IBikeService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,24 +21,33 @@ public class BikeService implements IBikeService {
     @Override
     public List<BikeDTO> allBikes() {
        return _bikeRepository.findAll().stream().map(bike -> new BikeDTO(
+               bike.getId(),
                bike.getMake(),
                bike.getPlateNumber(),
                bike.getKilometers())).collect(Collectors.toList());
     }
 
     @Override
-    public ResponseEntity<BikeDTO> registerBike(BikeDTO bikeDto) {
-        try{
-            _bikeRepository.save(new Bike(bikeDto.make()
-                    ,bikeDto.plateNumber(),
-                    bikeDto.kilometers()));
-
-            return new ResponseEntity<>(bikeDto, HttpStatus.CREATED);
-        }
-        catch(Exception ex)
-        {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public BikeDTO registerBike(BikeDTO bikeDto) {
+        _bikeRepository.save(new Bike(bikeDto.make()
+                ,bikeDto.plateNumber(),
+                bikeDto.kilometers()));
+        
+        return bikeDto;
     }
 
+    @Override
+    public BikeDTO getBike(Long bikeId) {
+        Bike bike = _bikeRepository.findById(bikeId).orElse(null);
+        
+        if(bike==null)
+        {
+            return  null;
+        }
+        return new BikeDTO(
+        bike.getId(),
+        bike.getMake(),
+        bike.getPlateNumber(),
+        bike.getKilometers());
+    }
 }
